ef31ad4e93ae28f38167f102dcba34e866df4cc8
bizhengyong <bizhengyong_xy@163.com>
2020-05-04 14:37:31 +0800

    5-6-获取商品列表（Feign）

--- src/main/java/com/imooc/product/controller/ProductController.java
+++ src/main/java/com/imooc/product/controller/ProductController.java
@@ -11,9 +11,7 @@ import com.imooc.product.vo.ResultVO;
 import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -72,5 +70,16 @@ public class ProductController {
         }
 
         return ResultVOUtil.success(productVOList);
+    }
+
+    /*
+     * @description: 获取商品列表（给订单服务用）
+     * @param:
+     * @author: bizy
+     * @date: 2020/5/4 13:47
+     */
+    @PostMapping("/listForOrder")
+    public List<ProductInfo> listForOrder(@RequestBody List<String> productIdList) {
+        return productService.findList(productIdList);
     }
 }
--- src/test/java/com/imooc/product/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/imooc/product/service/impl/ProductServiceImplTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -28,6 +29,12 @@ public class ProductServiceImplTest extends ProductApplicationTests {
     @Test
     public void findUpAll() {
         List<ProductInfo> list = productService.findUpAll();
+        Assert.assertTrue(list.size() > 0);
+    }
+
+    @Test
+    public void findList(){
+        List<ProductInfo> list = productService.findList(Arrays.asList("157875196366160022", "157875227953464068"));
         Assert.assertTrue(list.size() > 0);
     }
 }
