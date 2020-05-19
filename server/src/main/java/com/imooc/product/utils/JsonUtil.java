32513349258f2920babf810ebf0cd57fb8b34430
bizhengyong <bizhengyong_xy@163.com>
2020-05-19 20:12:03 +0800

    7-7-商品和订单服务中使用MQ(中)

--- server/src/main/java/com/imooc/product/utils/JsonUtil.java
+++ server/src/main/java/com/imooc/product/utils/JsonUtil.java
@@ -3,6 +3,8 @@ package com.imooc.product.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 /**
  * @author: bizy
  * @date: 2020/05/14 22:47
@@ -22,6 +24,22 @@ public class JsonUtil {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+    *@description: js转对象
+    *@param: 
+    *@return: 
+    *@author: bizy
+    *@date: 2020/5/19 19:09
+    */
+    public static Object fromJson(String string,Class classType) {
+        try {
+            return objectMapper.readValue(string,classType);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
